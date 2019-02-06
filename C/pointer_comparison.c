cc2d25ef144c42580fd22451fce9b75c115e7fa5
Mohammad Husein Amrullah <mhuseinamr@gmail.com>
2019-02-06 22:10:46 +0700

    Add pointer_comparison.c to C

---
+++ C/pointer_comparison.c
@@ -0,0 +1,31 @@
+/*
+A retarded program that simulates a for-loop with a while-loop
+and array access with pointer arithmetic.
+
+Reference:
+https://www.tutorialspoint.com/cprogramming/c_pointer_arithmetic.htm
+*/
+
+#include <stdio.h>
+
+const int MAX = 3;
+
+int main() {
+	int var[] = {10, 100, 200};
+	int i, *ptr;
+
+	/* let us have address of the first element in pointer */
+	ptr = var;
+	i = 0;
+
+	while (ptr <= &var[MAX - 1]) {
+		printf("Address of var[%d] = %x\n", i, ptr);
+		printf("Value of var[%d] = %d\n", i, *ptr);
+
+		/* point to the previous location */
+		ptr++;
+		i++;
+	}
+
+	return 0;
+}
